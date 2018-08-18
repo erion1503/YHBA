1f7d37ec6c178f3f5f620dbec729b7a6397bf07f
堀口 典靖 <muku_aya_ichigo_luna@hotmail.co.jp>
2018-08-19 03:18:10 +0900

    first commit.

---
+++ YHBA/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using YHBA.Views;
+using System.Windows;
+using Prism.Modularity;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace YHBA
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var moduleCatalog = (ModuleCatalog)ModuleCatalog;
+            //moduleCatalog.AddModule(typeof(YOUR_MODULE));
+        }
+    }
+}
